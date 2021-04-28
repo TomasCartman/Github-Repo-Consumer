df7ebf2eaacd9e9640b56abf96f7e4f180eba166
Tomas Cartman <marco12y@outlook.com>
2021-04-27 22:48:40 -0300

    add RepositoryDetailFragment to show details about the touched repository

--- app/src/main/java/com/blackpineapple/githubrepoconsumer/data/adapters/RepositoryListAdapter.kt
+++ app/src/main/java/com/blackpineapple/githubrepoconsumer/data/adapters/RepositoryListAdapter.kt
@@ -4,9 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.blackpineapple.githubrepoconsumer.R
 import com.blackpineapple.githubrepoconsumer.data.model.Repository
+import com.blackpineapple.githubrepoconsumer.ui.RepositoryDetailFragment
+import timber.log.Timber
 
 class RepositoryListAdapter(private val repoList: List<Repository>)
     : RecyclerView.Adapter<RepositoryListAdapter.RepositoryViewHolder>() {
@@ -20,14 +23,42 @@ class RepositoryListAdapter(private val repoList: List<Repository>)
     override fun onBindViewHolder(holder: RepositoryViewHolder, position: Int) {
         val repository = repoList[position]
 
+        Timber.d(repository.owner.ownerName)
+        Timber.d(repository.repoDescription)
+
         holder.repositoryNameTextView.text = repository.repoName
         holder.ownerNameTextView.text = repository.owner.ownerName
+        if(repository.repoDescription != null) {
+            holder.repositoryDescription = repository.repoDescription
+        }
+        holder.ownerAvatarUrl = repository.owner.ownerAvatarUrl
+
     }
 
     override fun getItemCount(): Int = repoList.size
 
-    inner class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    inner class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val repositoryNameTextView: TextView = itemView.findViewById(R.id.repository_name_textView)
         val ownerNameTextView: TextView = itemView.findViewById(R.id.owner_name_textView)
+        var repositoryDescription: String = ""
+        var ownerAvatarUrl: String = ""
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val activity = itemView.context as AppCompatActivity
+            val fragment = RepositoryDetailFragment.newInstance(
+                    repositoryNameTextView.text.toString(),
+                    repositoryDescription,
+                    ownerNameTextView.text.toString(),
+                    ownerAvatarUrl
+            )
+            activity.supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null).commit()
+        }
     }
+
 }
---
+++ app/src/main/java/com/blackpineapple/githubrepoconsumer/ui/RepositoryDetailFragment.kt
@@ -0,0 +1,53 @@
+package com.blackpineapple.githubrepoconsumer.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.blackpineapple.githubrepoconsumer.databinding.FragmentRepositoryDetailBinding
+import com.bumptech.glide.Glide
+
+class RepositoryDetailFragment : Fragment() {
+    private lateinit var binding: FragmentRepositoryDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentRepositoryDetailBinding.inflate(inflater, container, false)
+
+        val repoName = arguments?.getString(ARG_REPO_NAME) as String
+        val repoDescription = arguments?.getString(ARG_REPO_DESCRIPTION) as String
+        val ownerName = arguments?.getString(ARG_OWNER_NAME) as String
+        val ownerAvatarUrl = arguments?.getString(ARG_OWNER_AVATAR_URL) as String
+
+        binding.repoNameTextView.text = repoName
+        binding.repoDescriptionTextView.text = repoDescription
+        binding.ownerTextView.text = ownerName
+
+        context?.let {
+            Glide.with(it)
+                    .load(ownerAvatarUrl)
+                    .into(binding.ownerImageView)
+        }
+
+        return binding.root
+    }
+
+    companion object {
+        private const val ARG_REPO_NAME = "repoName"
+        private const val ARG_REPO_DESCRIPTION = "repoDescription"
+        private const val ARG_OWNER_NAME = "ownerName"
+        private const val ARG_OWNER_AVATAR_URL = "ownerAvatarUrl"
+
+
+        fun newInstance(repoName: String, repoDescription: String, ownerName: String, ownerAvatarUrl: String)
+        : RepositoryDetailFragment {
+            val args = Bundle().apply {
+                putString(ARG_REPO_NAME, repoName)
+                putString(ARG_REPO_DESCRIPTION, repoDescription)
+                putString(ARG_OWNER_NAME, ownerName)
+                putString(ARG_OWNER_AVATAR_URL, ownerAvatarUrl)
+            }
+            return RepositoryDetailFragment().apply { arguments = args }
+        }
+    }
+}
